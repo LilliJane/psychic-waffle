f0dd78dc2ff388d10336c2929b5608fd09a81d78
Emeline Gaulard <emeline.gaulard@epitech.eu>
2017-04-16 17:03:38 +0200

    add domain name to allowed hosts

--- example_app/settings.py
+++ example_app/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'fsch+6!=q+@ol&%0x!nwdl@48^ixbd4clx5f1i!5n^66y+pmn*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'talkingstatues.xyz']
 
 
 # Application definition
